5b0efc00ce3ac77fd4b4bc6d4a8039d8456722de
pgdeniv <pgdeniverville@gmail.com>
2017-05-31 11:11:16 +0900

    added constants.py

---
+++ constants.py
@@ -0,0 +1,41 @@
+#Units
+gev=1
+mev=1e-3*gev
+kev=1e-6*gev
+
+#Astrophysics
+h=0.678
+#energy density of the universe in GeV/cm^3
+rhocrit=1.053e-5*h**2
+#present day value of entropy
+entropy_today=2891.2
+OmegaCDM=0.1186*h**-2
+RhoCDM=rhocrit*OmegaCDM
+
+#masses
+mp=0.938272046*gev
+melec=511*kev
+mmuon=105.658*mev
+mpi=134.9767*mev
+mpip=139.57018*mev
+mkaon=0.493667*gev
+mj_psi=3.097*gev
+mmuon=0.1057*gev
+Mpl=1.220910e19*gev
+
+
+#Widths, Branching Ratios, Constants
+tauK=1.23e-8
+alpha_em=1.0/137.035999139
+Brj_psi_to_ee=0.0594
+Brj_psi_to_mumu=0.0593
+Brj_psi_to_invis=7e-4
+
+hbar=float(1.054*1e-34/(1.6e-19)/(1e9))
+speed_of_light=3e8
+conversion=hbar**2*speed_of_light**2
+
+
+#Relic density cross section for rough estimates of relic density
+relic_density_sigma=1e-40
+
